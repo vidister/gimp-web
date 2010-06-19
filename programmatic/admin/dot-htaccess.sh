2429ebb768578695bed30feb2cad0e30639c8f7f
Jakub Steiner <jimmac@novell.com>
2010-06-19 21:31:02 +0200

    doesn't even 'build' for me. throw everything out and start from scratch.

--- programmatic/admin/dot-htaccess.sh
+++
@@ -1,7 +0,0 @@
-AddHandler cgi-script .cgi
-AuthName "news.gimp.org"
-AuthType Digest
-AuthDigestFile __NEWSDIR__/.password
-AuthDigestDomain /news
-Options +Includes +Indexes +ExecCGI +MultiViews +SymLinksIfOwnerMatch +IncludesNoExec
-Require valid-user
