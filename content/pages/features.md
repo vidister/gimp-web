61e5c7867be17ac8c2f75178f5f81cd8e30fd0ed
Pat David <patdavid@gmail.com>
2015-07-31 15:00:12 -0500

    Initial upload of new data
    Signed-off-by: Pat David <patdavid@gmail.com>

---
+++ content/pages/features.md
@@ -0,0 +1,12 @@
+Title: Features
+Date: 2015-07-29T14:40:35-05:00
+Modified: 2015-07-29T14:40:43-05:00
+Authors: Pat David
+Summary: A look at some high level features of GIMP.
+
+Content area of the *Features* page markdown
+
+Just filling out some information on an _Features_ page to have something to style against.
+
+### Heading Three
+Text just under heading three (3).
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Pat David'
+SITENAME = u'GIMP'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+PAGE_URL = "{slug}/"
+PAGE_SAVE_AS = "{slug}/index.html"
+
+ARTICLE_URL = "news/{date:%Y}/{date:%m}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = "news/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
+
+TYPOGRIFY = False
